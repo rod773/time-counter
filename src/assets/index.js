5c82cafcb51c745abc83de5d4947dd3712608285
rod773 <rodrigoarcadio1@gmail.com>
2024-06-03 09:39:00 +0200

    update

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import play from "./play.svg";
+import pause from "./pause.svg";
+import stop from "./stop.svg";
+
+const images = {
+  play: play,
+  pause: pause,
+  stop: stop,
+};
+
+export { images };
