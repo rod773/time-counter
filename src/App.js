b193e8e661dd97722a085d4e8afa737ead668499
rod773 <rodrigoarcadio1@gmail.com>
2024-06-03 09:27:20 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Counter from "Counter";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  state = {
+    horaInicio: 0,
+    horaFin: 0,
+    total: 0,
+    start: false,
+    count: "",
+    milisec: 0,
+    inicio: 0,
+  };
+
+  offset = new Date().getTimezoneOffset() / 2;
+
+  startTimer = () => {
+    if (this.state.milisec === 0)
+      this.setState({ horaInicio: new Date().getTime() });
+    setInterval(() => {
+      if (this.state.start) {
+        this.setState((state) => ({
+          milisec: new Date().getTime() - this.state.horaInicio,
+          count: new Date(
+            this.state.milisec + this.offset * 60000
+          ).toLocaleTimeString("en-GB"),
+        }));
+      }
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  render() {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-xl font-bold">Counter</h1>
+        <h2 className="text-xl font-semibold">{this.state.count}</h2>
+        <h2 className="text-xl font-semibold">{this.state.milisec}</h2>
+        <div className="flex justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  mx-4"
+            onClick={() => {
+              this.setState({ start: !this.state.start });
+            }}
+          >
+            start
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  mx-4"
+            onClick={() => {
+              this.setState({ count: 0, milisec: 0 });
+            }}
+          >
+            reset
+          </button>
+        </div>
+
+        <div className="text-xl font-semibold">
+          timezone offset : {this.offset} min
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
